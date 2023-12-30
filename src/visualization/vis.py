6dd6e36ea1d78ef2fc988fa2a42ee33aef7c5e7c
Abhimanyu Suthar <71772205+Abhimanyu-0@users.noreply.github.com>
2023-12-29 21:53:38 -0500

    Update vis.py

--- src/visualization/vis.py
+++ src/visualization/vis.py
@@ -4,7 +4,7 @@ import pybullet as p
 p.connect(p.GUI)
 
 #Add your own path
-robot_urdf_path = "/Users/abhimanyusuthar/anaconda3/Robotics/gitrepo/bullet3/examples/pybullet/gym/pybullet_data/xarm/xarm6_robot.urdf"
+robot_urdf_path = "~/gitrepo/bullet3/examples/pybullet/gym/pybullet_data/xarm/xarm6_robot.urdf"
 robot_id = p.loadURDF(robot_urdf_path, useFixedBase=Trueimport time
 initial_joint_angles = [m.radians(0), m.radians(30), m.radians(0), m.radians(-180), m.radians(0), m.radians(90),m.radians(0)] # This is the home configuration(according to my DH table)
 for i in range(0,p.getNumJoints(robot_id)-1):
